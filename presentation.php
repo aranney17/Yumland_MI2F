c308f320339e91b5e2af2a707c8166140b51b680
sr04x <sarah19042006@gmail.com>
2026-04-05 22:48:19 +0200

    Update profile menu for user authentication

--- presentation.php
+++ presentation.php
@@ -131,9 +131,16 @@ if (isset($_POST['ajouter_panier'])) {
         <img src="images/Iconprofil.png" alt="Profil" class="icon">
 
         <div class="profil-bulle">
-            <a href="inscription.html">Inscription</a>
-            <a href="connexion.html">Connexion</a>
-        </div>
+    <?php if (isset($_SESSION["connecte"]) && $_SESSION["connecte"]): ?>
+        <!-- Utilisateur connecté -->
+        <a href="profil.php">Mon profil</a>
+        <a href="logout.php">Se déconnecter</a>
+    <?php else : ?>
+        <!-- Visiteur -->
+        <a href="inscription.php">S'Inscrire</a>
+        <a href="connexion.php">Connexion</a>
+    <?php endif; ?>
+</div>
     </div>
 
 
