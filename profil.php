783b1d1853ee97bcd131e9af8d16e8ceab490017
sr04x <sarah19042006@gmail.com>
2026-05-17 10:44:36 +0200

    Refactor profil.php for improved readability and security

--- profil.php
+++ profil.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
 
-// Vérifier si utilisateur connecté
 if (!isset($_SESSION['id'])) {
     header("Location: connexion.php");
     exit();
 }
 
-// Charger JSON
-$json = file_get_contents("data/infoclient.json");
-$users = json_decode($json, true);
-
-// Trouver l'utilisateur connecté
+$users = json_decode(file_get_contents("data/infoclient.json"), true) ?? [];
 $id = $_SESSION['id'];
 $userTrouve = null;
-
 foreach ($users as $user) {
-    if ($user['id'] == $id) {
-        $userTrouve = $user;
-        break;
-    }
+    if ($user['id'] == $id) { $userTrouve = $user; break; }
 }
 ?>
 
@@ -31,18 +22,13 @@ foreach ($users as $user) {
     <link rel="stylesheet" href="couleurs.css">
     <link rel="stylesheet" href="structg.css">
     <link rel="stylesheet" href="profil.css">
+    <link rel="stylesheet" href="darkmode.css">
 </head>
 <body>
 
 <header>
-    <div class="barres">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-
+    <div class="barres"><span></span><span></span><span></span></div>
     <h1><a href="accueil.php" class="logo">La Cour des Délices</a></h1>
-
     <div class="top-icons">
         <div class="profil-menu">
             <img src="images/Iconprofil.png" class="icon">
@@ -58,7 +44,7 @@ foreach ($users as $user) {
 
 <aside class="sidebar">
     <ul class="menu">
-        <li><a href="profil.php">Informations</a></li>
+        <li><a href="profil.php"><strong>Informations</strong></a></li>
         <li><a href="profil2.php">Historique de commandes</a></li>
         <li>Données personnelles</li>
     </ul>
@@ -71,40 +57,45 @@ foreach ($users as $user) {
 <?php if ($userTrouve): ?>
 
 <h2 class="title">
-    Informations  
+    Informations
     <a href="modifier_profil.php"><img src="images/crayon.png" width="15"></a>
 </h2>
 
 <div class="block">Civilité :
-    <br><?= $userTrouve['civilite'] ?? '' ?>
+    <br><?= htmlspecialchars($userTrouve['civilite'] ?? '') ?>
 </div>
 
 <span class="inline">Nom :
-    <br><?= $userTrouve['nom'] ?>
+    <br><?= htmlspecialchars($userTrouve['nom']) ?>
 </span>
 
 <span class="inline">Prénom :
-    <br><?= $userTrouve['prenom'] ?>
+    <br><?= htmlspecialchars($userTrouve['prenom']) ?>
 </span>
 
 <div class="block">Date de naissance :
-    <br><?= $userTrouve['date_naissance'] ?? '' ?>
+    <br><?= htmlspecialchars($userTrouve['date_naissance'] ?? '') ?>
 </div>
 
 <div class="block">Téléphone :
-    <br><?= $userTrouve['telephone'] ?? '' ?>
+    <br><?= htmlspecialchars($userTrouve['telephone'] ?? '') ?>
+</div>
+
+<!-- AFFICHAGE ADRESSE SEPAREE -->
+<div class="block">Rue :
+    <br><?= htmlspecialchars($userTrouve['rue'] ?? '') ?>
 </div>
 
 <div class="block">Code postal :
-    <br><?= $userTrouve['code_postal'] ?? '' ?>
+    <br><?= htmlspecialchars($userTrouve['code_postal'] ?? '') ?>
 </div>
 
-<div class="block">Adresse :
-    <br><?= $userTrouve['adresse'] ?? '' ?>
+<div class="block">Ville :
+    <br><?= htmlspecialchars($userTrouve['ville'] ?? '') ?>
 </div>
 
 <div class="block">Adresse mail :
-    <br><?= $userTrouve['mail'] ?>
+    <br><?= htmlspecialchars($userTrouve['mail']) ?>
 </div>
 
 <div class="block">Mot de passe :
@@ -119,24 +110,19 @@ foreach ($users as $user) {
 </main>
 
 <footer>
-    <p>Suivez nous sur nos réseaux!
-        <br>
-        <img src="images/Iconinstagram.jpg" alt="instagram" class="icon">
-        <img src="images/Icontiktok.jpg" alt="tiktok" class="icon">
-        <img src="images/Iconinstagram.jpg" alt="instagram" class="icon">
+    <p>Suivez nous sur nos réseaux!<br>
+        <img src="images/Iconinstagram.jpg" class="icon">
+        <img src="images/Icontiktok.jpg" class="icon">
+        <img src="images/Icontwitter.png" class="icon">
     </p>
     <div class="infos-footer">
-        <div class="info">
-            <img src="images/Iconlocalisation.png" alt="maps" class="icon">
-            <span>5 av de la république, 75300 Paris</span>
-        </div>
-        <div class="info">
-            <img src="images/Iconhorloge.png" alt="horloge" class="icon">
-            <span>Tous les jours 9h - 22h</span>
-        </div>
+        <div class="info"><img src="images/Iconlocalisation.png" class="icon"><span>5 av de la république, 75300 Paris</span></div>
+        <div class="info"><img src="images/Iconhorloge.png" class="icon"><span>Tous les jours 9h - 22h</span></div>
     </div>
-    <h5>© 2026 Pâtisserie</h5>    
+    <h5>© 2026 Pâtisserie</h5>
 </footer>
 
+<button id="btn-darkmode" class="btn-darkmode">☾</button>
+<script src="darkmode.js"></script>
 </body>
 </html>
