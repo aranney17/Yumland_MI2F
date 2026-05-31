2810d0524d8b2582a93cf6e8ad11455e7075abb3
sr04x <sarah19042006@gmail.com>
2026-05-31 19:45:07 +0200

    Update file paths in livraison.php for directory structure

--- fichiers_php/livraison.php
+++ fichiers_php/livraison.php
@@ -6,11 +6,11 @@ session_start();
 
 /*  SECURITE : reserve aux livreurs + check si bloque */
 if (!isset($_SESSION['id'])) {
-    header("Location: fichiers_php/connexion.php");
+    header("Location: ../fichiers_php/connexion.php");
     exit();
 }
 
-$clients = json_decode(file_get_contents('data/infoclient.json'), true) ?? [];
+$clients = json_decode(file_get_contents('../data/infoclient.json'), true) ?? [];
 $roleUser = null;
 $userBloque = false;
 foreach ($clients as $c) {
@@ -23,7 +23,7 @@ foreach ($clients as $c) {
 
 if ($userBloque) {
     session_destroy();
-    die("Votre compte a été bloqué. <a href='fichiers_php/connexion.php'>Retour à la connexion</a>");
+    die("Votre compte a été bloqué. <a href='../fichiers_php/connexion.php'>Retour à la connexion</a>");
 }
 
 if ($roleUser !== 'livreur') {
@@ -32,7 +32,7 @@ if ($roleUser !== 'livreur') {
 }
 
 /* Logique livraison*/
-$commandes = json_decode(file_get_contents('data/commande.json'), true) ?? [];
+$commandes = json_decode(file_get_contents('../data/commande.json'), true) ?? [];
 
 if (isset($_POST['start_id'])) {
     foreach ($commandes as &$cmd) {
@@ -41,8 +41,8 @@ if (isset($_POST['start_id'])) {
         }
     }
     unset($cmd);
-    file_put_contents('data/commande.json', json_encode($commandes, JSON_PRETTY_PRINT));
-    header('Location: fichiers_php/livraison.php');
+    file_put_contents('../data/commande.json', json_encode($commandes, JSON_PRETTY_PRINT));
+    header('Location: ../fichiers_php/livraison.php');
     exit;
 }
 
@@ -54,8 +54,8 @@ if (isset($_POST['finish_id'])) {
         }
     }
     unset($cmd);
-    file_put_contents('data/commande.json', json_encode($commandes, JSON_PRETTY_PRINT));
-    header('Location: fichiers_php/livraison.php');
+    file_put_contents('../data/commande.json', json_encode($commandes, JSON_PRETTY_PRINT));
+    header('Location: ../fichiers_php/livraison.php');
     exit;
 }
 
@@ -75,23 +75,23 @@ $commandes_filtrees = array_filter($commandes, function($cmd) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livraison</title>
-    <link rel="stylesheet" href="fichiers_css/couleurs.css">
-    <link rel="stylesheet" href="fichiers_css/structg.css">
-    <link rel="stylesheet" href="fichiers_css/livraison.css">
-    <link rel="stylesheet" href="fichiers_css/darkmode.css">
+    <link rel="stylesheet" href="../fichiers_css/couleurs.css">
+    <link rel="stylesheet" href="../fichiers_css/structg.css">
+    <link rel="stylesheet" href="../fichiers_css/livraison.css">
+    <link rel="stylesheet" href="../fichiers_css/darkmode.css">
 </head>
 <body>
 
 <div class="barres"><span></span><span></span><span></span></div>
 
-<h1><a href="fichiers_php/livraison.php" class="logo">La Cour des Délices</a></h1>
+<h1><a href="../fichiers_php/livraison.php" class="logo">La Cour des Délices</a></h1>
 
 <div class="top-icons">
     <div class="profil-menu">
-        <img src="images/Iconprofil.png" class="icon">
+        <img src="../images/Iconprofil.png" class="icon">
         <div class="profil-bulle">
-            <a href="fichiers_php/profil.php">Profil</a>
-            <a href="fichiers_php/logout.php">Déconnexion</a>
+            <a href="../fichiers_php/profil.php">Profil</a>
+            <a href="../fichiers_php/logout.php">Déconnexion</a>
         </div>
     </div>
 </div>
@@ -104,7 +104,7 @@ $commandes_filtrees = array_filter($commandes, function($cmd) {
         <a class="adresse"
            href="https://www.google.com/maps/search/?api=1&query=<?= urlencode($commande_en_cours['adresse']) ?>"
            target="_blank">
-            <img src="images/Iconlocalisation.png" class="map-icon">
+            <img src="../images/Iconlocalisation.png" class="map-icon">
             <span><?= $commande_en_cours['adresse'] ?></span>
         </a>
         <div class="infos">
@@ -125,7 +125,7 @@ $commandes_filtrees = array_filter($commandes, function($cmd) {
         <a class="adresse"
            href="https://www.google.com/maps/search/?api=1&query=<?= urlencode($cmd['adresse']) ?>"
            target="_blank">
-            <img src="images/Iconlocalisation.png" class="map-icon">
+            <img src="../images/Iconlocalisation.png" class="map-icon">
             <span><?= $cmd['adresse'] ?></span>
         </a>
         <div class="infos">
@@ -142,19 +142,19 @@ $commandes_filtrees = array_filter($commandes, function($cmd) {
 
 <footer>
 <p>suivez nous sur nos réseaux!<br>
-    <img src="images/Iconinstagram.jpg" class="icon">
-    <img src="images/Icontiktok.jpg" class="icon">
-    <img src="images/Icontwitter.png" class="icon">
+    <img src="../images/Iconinstagram.jpg" class="icon">
+    <img src="../images/Icontiktok.jpg" class="icon">
+    <img src="../images/Icontwitter.png" class="icon">
 </p>
 <div class="infos-footer">
-    <div class="info"><img src="images/Iconlocalisation.png" class="icon"><span>5 avenue de la république, 75015 Paris</span></div>
-    <div class="info"><img src="images/Iconhorloge.png" class="icon"><span>Tous les jours 9h - 20h</span></div>
+    <div class="info"><img src="../images/Iconlocalisation.png" class="icon"><span>5 avenue de la république, 75015 Paris</span></div>
+    <div class="info"><img src="../images/Iconhorloge.png" class="icon"><span>Tous les jours 9h - 20h</span></div>
 </div>
 <h5>© 2026 Pâtisserie</h5>
 </footer>
 
 <button id="btn-darkmode" class="btn-darkmode">☾</button>
-<script src="darkmode.js"></script>
+<script src="../fichiers_js/darkmode.js"></script>
 </body>
 </html>
 
