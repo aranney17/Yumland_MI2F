a9099e59f665058602c4ed77e28c65a9b1f8952b
syrine-chellat <syrine.chellat@gmail.com>
2026-06-10 10:27:15 +0200

    Disable user role redirection check
    Comment out role check for user redirection

--- fichiers_php/produits.php
+++ fichiers_php/produits.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['id'])) {
                 session_destroy();
                 die("Votre compte a été bloqué. <a href='../connexion.php'>Retour</a>");
             }
-            if ($u['role'] !== 'client') {
+            /*if ($u['role'] !== 'client') {
                 $redir = [
                     'cuisinier'      => '../fichiers_php/commandes.php',
                     'livreur'        => '../fichiers_php/livraison.php',
@@ -18,7 +18,7 @@ if (isset($_SESSION['id'])) {
                 ];
                 header("Location: " . ($redir[$u['role']] ?? '../fichiers_php/accueil.php'));
                 exit();
-            }
+            }*/
             break;
         }
     }
